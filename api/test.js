3e308d0b12f7647a793183d14260126c8c5a9106
Amely <amely.attias@gmail.com>
2026-06-08 14:04:00 +0000

    debug: test minimal + supprimer config functions vercel.json

--- api/test.js
+++ api/test.js
@@ -1,12 +1,3 @@
-const { Client } = require('@notionhq/client')
-
-module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  try {
-    const notion = new Client({ auth: process.env.NOTION_KEY })
-    const db = await notion.databases.retrieve({ database_id: 'ce868414-a4a5-4450-ab3f-804be7fd5eb1' })
-    res.status(200).json({ ok: true, title: db.title?.[0]?.plain_text, node: process.version })
-  } catch(e) {
-    res.status(500).json({ error: e.message, node: process.version })
-  }
+module.exports = function handler(req, res) {
+  res.status(200).json({ ok: true, node: process.version, key: !!process.env.NOTION_KEY })
 }
